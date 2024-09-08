cb945a9a5534d11d97b0445eda1f3818a1ab607b
GUGULOTH PAVAN KALYAN <140972405+pavankalyan3699@users.noreply.github.com>
2024-09-08 11:30:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+fetch("Dailyexpenses.json")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        let placeholder = document.querySelector("#data-output");
+        let out = "";
+
+        data["Form Responses 1"].forEach(expense => {
+            out += `
+                <tr>
+                    <td>${expense.SI_No}</td>
+                    <td>${expense.Date}</td>
+                    <td>${expense.Purpose}</td>
+                    <td>${expense.Amount}</td>
+                    <td><a href="${expense.BillURL}" target="_blank">View Bill</a></td>
+                </tr>
+            `;
+        });
+
+        placeholder.innerHTML = out;
+    })
+    .catch(error => {
+        console.error('Error loading JSON data:', error);
+    });
